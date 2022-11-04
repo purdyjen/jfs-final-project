5a8b401a2aad06df91e6ac6ded45e84785d786a6
purdyjen <purdyjen@me.com>
2022-11-04 09:46:06 -0400

    modularized project

--- js/productsController.js
+++ js/productsController.js
@@ -1,5 +1,5 @@
 console.log("Controller connected!");
-class ProductsController {
+export default class ProductsController {
   constructor(currentId = 0) {
     this.products = [];
     this.currentId = currentId;
@@ -30,18 +30,3 @@ class ProductsController {
     this.products.push(product);
   }
 }
-
-const products = new ProductsController();
-products.addProduct(
-  "brand dog food",
-  "dog food",
-  "/images/products/dog-food-5168940_640.jpg",
-  15
-);
-products.addProduct(
-  "brand cat food",
-  "cat food",
-  "/images/products/cat-2170495_640.jpg",
-  15
-);
-console.log(products.products);
