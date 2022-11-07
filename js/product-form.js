193ac5af07b4833922e665851639979107388183
purdyjen <purdyjen@me.com>
2022-11-07 14:51:10 -0500

    added notes to product-form.js

--- js/product-form.js
+++ js/product-form.js
@@ -1,36 +1,40 @@
+// Imports class ProductsController
 import ProductsController from "./ProductsController.js";
+// Imports two "helper functions" to use in validating/formatting input values
 import { titleCase, formatPrice } from "./helperFunctions.js";
-// Initialize a new productsController with currentId set to 0
+// Initialize a new productsController object
 const productsController = new ProductsController();
+// Loads the products from localStorage
 productsController.loadProductsFromLS();
-// Select the new product form
+// Selects the new product form element to add an event listener
 const addNewProductForm = document.getElementById("add-new-product");
-// Add an onsubmit event listener
-
+// Adds an onsubmit event listener
 addNewProductForm.addEventListener("submit", (e) => {
-  // Prevent default action
+  // Prevents default page reload action on submit event
   e.preventDefault();
-  // Select the inputs
+  // Selects the inputs
   const newProductName = document.getElementById("product-name");
   const newProductDescription = document.getElementById("product-description");
   const newProductImageUrl = document.getElementById("product-image-url");
   const newProductImageAltText = document.getElementById("image-alt-text");
   const newProductPrice = document.getElementById("product-price");
-  // Get the values of the inputs
+  // Gets the values of the inputs
   let name = newProductName.value;
   let description = newProductDescription.value;
   let imageUrl = newProductImageUrl.value;
   let imageAltText = newProductImageAltText.value;
   let price = Number(newProductPrice.value);
+
   // Validation Code
 
   // Name Validation - desired output is title case
+  // Uses titleCase helper function to ensure value of name is title cased
   name = titleCase(name);
 
-  // Format price to have two decimals
+  // Formats price to have two decimals
   price = formatPrice(price);
 
-  // Add product to the ProductsController
+  // Adds new product to the ProductsController products array using the addProduct method
   productsController.addProduct(
     name,
     description,
@@ -38,9 +42,9 @@ addNewProductForm.addEventListener("submit", (e) => {
     imageAltText,
     price
   );
-  productsController.saveProductsToLS();
+  productsController.saveProductsToLS(); // Updates saved products value in localStorage
 
-  // Clear the form
+  // Clears the form
   newProductName.value = "";
   newProductDescription.value = "";
   newProductImageUrl.value = "";
